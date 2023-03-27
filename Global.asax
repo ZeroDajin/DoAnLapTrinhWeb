913cd03fe81dc41c3d66a48e7b0153c47331653c
ZeroDajin <126646596+ZeroDajin@users.noreply.github.com>
2023-03-27 20:57:26 +0700

    Start

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAnWebTruyenTranh.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnWebTruyenTranh.Startup))]
+namespace DoAnWebTruyenTranh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
