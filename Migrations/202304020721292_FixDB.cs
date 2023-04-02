b91d298abf83dc7ec5eef9c6c1295b9b6a6d1abb
ZeroDajin <126646596+ZeroDajin@users.noreply.github.com>
2023-04-02 19:37:42 +0700

    I WANT TO DIEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ Migrations/202304020721292_FixDB.cs
@@ -0,0 +1,18 @@
+﻿namespace DoAnWebTruyenTranh.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixDB : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Trangs", "TSequence", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Trangs", "TSequence");
+        }
+    }
+}
